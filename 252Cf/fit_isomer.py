ac7600f44fe022b9ea852dd25c59a42bcfd55fc4
Dorthea Gjestvang <dortheagjestvang@Dortheas-MacBook-Air.local>
2021-11-24 15:54:17 +0100

    Added uncertainty calculation for IYR doublegate, now implementing extraction for singlegates as well

--- 252Cf/fit_isomer.py
+++ 252Cf/fit_isomer.py
@@ -13,11 +13,26 @@ import time
 ##     Read in data     ## 
 ##########################
 
-file = ROOT.TFile.Open("252Cf_23nov2021.root"," READ ")
+file = ROOT.TFile.Open("252Cf_24nov2021.root"," READ ")
 
-#####  134Te #####
+#####################
+###     134Te      ##
+#####################
 
-#Doublegate
+#Isomer_1 gate (297keV) true
+hist_isomer_1_gate_134Te = file.Get('time_isomer_1_gate_134Te')
+x_bins = hist_isomer_1_gate_134Te.GetNbinsX()
+
+x_isomer_1_gate_134Te = np.zeros(x_bins)
+y_isomer_1_gate_134Te = np.zeros(x_bins)
+
+for i in range(x_bins):
+    x_isomer_1_gate_134[i] = hist_isomer_1_gate_134Te.GetBinCenter(i+1)
+    y_isomer_1_gate_134[i] = hist_isomer_1_gate_134Te.GetBinContent(i+1)
+
+#######################
+
+#Doublegate true
 hist_doublegate_134Te = file.Get('time_isomer_doublegate_134Te')
 x_bins = hist_doublegate_134Te.GetNbinsX()
 
@@ -50,9 +65,7 @@ for i in range(x_bins):
     x_doublegate_bg_134Te[i] = hist_doublegate_bg_134Te.GetBinCenter(i+1)
     y_doublegate_bg_134Te[i] = hist_doublegate_bg_134Te.GetBinContent(i+1)
 
-##############
-
-M_gate_double, C_gate_double, x_gate_double = read_mama_1D("252Cf_time_isomer_gate_double_134Te_18may2021.m")
+############################
 
 
 ###################################
@@ -99,8 +112,8 @@ print(str(func))
 
 #Remember: will need some help to find correct fit parameters
 #amplitude_conv, mean, sigma, amplitude_gauss, amplitude_exp, amplitude_exp2
-#P, cov = curve_fit(func, x_gate_double, M_gate_double, bounds=([0,970,5,20,10,0],[100,1100,20,100,50,50]))
 
+#134Te doublegate fits
 P, cov = curve_fit(func, x_doublegate_134Te, y_doublegate_134Te, bounds=([0,950,0,20,10,0],[1000,1100,40,300,200,50]))
 print("\n")
 print(" *****  True spectrum fit ***** \n")
@@ -111,26 +124,29 @@ print("amplitude_gauss: %.4f" % P[3])
 print("amplitude_exp: %.4f" % P[4])
 print("amplitude_exp2: %.4f" % P[5])
 
-# P_all, cov_all = curve_fit(func, x_doublegate_all_134Te, y_doublegate_all_134Te, bounds=([0,950,0,20,10,0],[1000,1100,40,300,200,50]))
-# print("\n")
-# print(" *****  All spectrum fit ***** \n")
-# print("amplitude_conv: %.4f" % P_all[0])
-# print("mean: %.4f" % P_all[1])
-# print("sigma: %.4f" % P_all[2])
-# print("amplitude_gauss: %.4f" % P_all[3])
-# print("amplitude_exp: %.4f" % P_all[4])
-# print("amplitude_exp2: %.4f" % P_all[5])
+P_all, cov_all = curve_fit(func, x_doublegate_all_134Te, y_doublegate_all_134Te, bounds=([0,950,0,20,10,0],[1000,1100,40,300,200,50]))
+print("\n")
+print(" *****  All spectrum fit ***** \n")
+print("amplitude_conv: %.4f" % P_all[0])
+print("mean: %.4f" % P_all[1])
+print("sigma: %.4f" % P_all[2])
+print("amplitude_gauss: %.4f" % P_all[3])
+print("amplitude_exp: %.4f" % P_all[4])
+print("amplitude_exp2: %.4f" % P_all[5])
 
 
-# P_bg, cov_bg = curve_fit(func, x_doublegate_bg_134Te, y_doublegate_bg_134Te, bounds=([0,950,0,0,0,0],[1000,1100,40,40,40,50]))
-# print("\n")
-# print(" *****  BG spectrum fit ***** \n")
-# print("amplitude_conv: %.4f" % P_bg[0])
-# print("mean: %.4f" % P_bg[1])
-# print("sigma: %.4f" % P_bg[2])
-# print("amplitude_gauss: %.4f" % P_bg[3])
-# print("amplitude_exp: %.4f" % P_bg[4])
-# print("amplitude_exp2: %.4f" % P_bg[5])
+P_bg, cov_bg = curve_fit(func, x_doublegate_bg_134Te, y_doublegate_bg_134Te, bounds=([0,950,0,0,0,0],[1000,1100,40,40,40,50]))
+print("\n")
+print(" *****  BG spectrum fit ***** \n")
+print("amplitude_conv: %.4f" % P_bg[0])
+print("mean: %.4f" % P_bg[1])
+print("sigma: %.4f" % P_bg[2])
+print("amplitude_gauss: %.4f" % P_bg[3])
+print("amplitude_exp: %.4f" % P_bg[4])
+print("amplitude_exp2: %.4f" % P_bg[5])
+
+
+
 
 ##########################
 ## 		Chi squared 	## 
@@ -147,37 +163,60 @@ degrees of freedom = (#rows-1)*(#colums-1) = (len(M_gate_double[1990:2300])-1)*1
 #print("\n")
 #print("Chi^2 from %.d ns to %.d ns is %.3f, p-value is %.6f, df is %.d" % (x_gate_double[1990], x_gate_double[2300], chi2, p_value, len(M_gate_double[1990:2300])-1 ))
 
-##########################
-## 		Find IYR 		## 
-##########################
 
-x_arr = np.linspace(900,4000,4000)
+######################################
+## 		 Find IYR + uncertainty 	## 
+######################################
 
-area_tot = np.trapz(func(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), x_arr)
-area_gauss = np.trapz(gauss(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), x_arr)
-area_exp = np.trapz(smeared_exp(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), x_arr)
+x_arr = np.linspace(0,3000,3000)
 
-IYR = (area_exp)/(2*area_gauss + area_exp)
+area_true = np.trapz(func(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), x_arr)
+area_true_prompt = np.trapz(gauss(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), x_arr)
+area_true_delayed = np.trapz(smeared_exp(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), x_arr)
+
+print(area_true)
+print(area_true_prompt)
+print(area_true_delayed)
+
+area_all = np.trapz(func(x_arr, P_all[0], P_all[1], P_all[2], P_all[3], P_all[4], P_all[5]), x_arr)
+area_all_prompt = np.trapz(gauss(x_arr, P_all[0], P_all[1], P_all[2], P_all[3], P_all[4], P_all[5]), x_arr)
+area_all_delayed = np.trapz(smeared_exp(x_arr, P_all[0], P_all[1], P_all[2], P_all[3], P_all[4], P_all[5]), x_arr)
+
+area_bg = np.trapz(func(x_arr, P_bg[0], P_bg[1], P_bg[2], P_bg[3], P_bg[4], P_bg[5]), x_arr)
+area_bg_prompt = np.trapz(gauss(x_arr, P_bg[0], P_bg[1], P_bg[2], P_bg[3], P_bg[4], P_bg[5]), x_arr)
+area_bg_delayed = np.trapz(smeared_exp(x_arr, P_bg[0], P_bg[1], P_bg[2], P_bg[3], P_bg[4], P_bg[5]), x_arr)
+
+IYR = (area_true_delayed)/(2*area_true_prompt + area_true_delayed)
+
+
+sigma_area_true_prompt = np.sqrt(area_all_prompt + area_bg_prompt)
+sigma_area_true_delayed = np.sqrt(area_all_delayed + area_bg_delayed)
+
+p = area_true_prompt
+d = area_true_delayed
+sigma_p = sigma_area_true_prompt
+sigma_d = sigma_area_true_delayed
+
+sigma_IYR = np.sqrt( ((2*p/(2*p+d)**2)*sigma_d)**2 + ((-2*d/(2*p+d)**2)*sigma_p)**2 )
+
 print("\n")
-print("*************** \n")
-print("total area of true fit: %.6f" % area_tot)
-print("gauss area of true fit: %.6f" % area_gauss)
-print("exp area of true fit: %.6f" % area_exp)
-print("IYR: %.6f " % IYR)
+print(" ***** Isomeric Yield Ration ****")
+print("IYR: %.4f +/- %.4f " % (IYR,sigma_IYR) )
 print("\n")
+
 
 ##########################
 ## 			Plot 		## 
 ##########################
 
-plt.plot(x_doublegate_134Te, y_doublegate_134Te, label="doublegate_134Te", color="blue")
+#plt.errorbar(x_doublegate_134Te, y_doublegate_134Te, yerr=np.sqrt(abs(y_doublegate_134Te)),fmt=".", label="doublegate_134Te", color="royalblue")
+plt.plot(x_doublegate_134Te, y_doublegate_134Te, label="doublegate_134Te", color="royalblue")
 #plt.plot(x_doublegate_all_134Te, y_doublegate_all_134Te, label="doublegate_all_134Te", color="black")
 #plt.plot(x_doublegate_bg_134Te, y_doublegate_bg_134Te, label="doublegate_bg_134Te", color="pink")
 
 plt.plot(x_arr, func(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), label="true fit, total", color="orange")
 plt.plot(x_arr, gauss(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), label="true gaussian", color="green")
 plt.plot(x_arr, smeared_exp(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), label="true smeared exp", color="red")
-plt.plot(x_arr, gauss(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]) + smeared_exp(x_arr, P[0], P[1], P[2], P[3], P[4], P[5]), label="sum gauss + smeared exp", color="purple")
 
 # plt.plot(x_arr, func(x_arr, P_all[0], P_all[1], P_all[2], P_all[3], P_all[4], P_all[5]), label="all fit, total", color="orange")
 # plt.plot(x_arr, gauss(x_arr, P_all[0], P_all[1], P_all[2], P_all[3], P_all[4], P_all[5]), label="all gaussian", color="green")
