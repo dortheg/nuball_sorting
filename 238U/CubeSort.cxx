d38c9c598c1818e80f09f2d2184043cd124690f0
Dorthea Gjestvang <dortheagjestvang@Dortheas-MacBook-Air.local>
2022-01-05 10:14:24 +0100

    Created lookuptable in 2D, with differentiation between bg_ridge and bg_random

--- 238U/CubeSort.cxx
+++ 238U/CubeSort.cxx
@@ -37,9 +37,9 @@ using namespace std;
 int TBINS=350;
 int EBINS=2048;
 
-short lookup[2048][2048];
+short lookup[2048][2048] = {0};
 int coincidences[20][2];
-int lookup1D[2048];
+int lookup1D[2048] = {0};
 
 //Number of FWHM in the energy gate
 double NFWHM=1.0;
@@ -47,11 +47,11 @@ double NFWHM=1.0;
 
 int main(int argc, char **argv){
 
-	//Create & read in cubes
+/*	//Create & read in cubes
 	CubeDDT *Cube1=new CubeDDT("",EBINS,TBINS,10);
 	CubeDDT *Cube2=new CubeDDT("",EBINS,TBINS,10);
 	Cube1->Read("U238cube_n3_4jan2021.bin");
-	Cube2->Read("U238cube_n3_4jan2021-sub.bin");
+	Cube2->Read("U238cube_n3_4jan2021-sub.bin");*/
 
 	// Create coincidence matrix
 	TH2F* ggm = new TH2F("ggm","ggm",EBINS,0,EBINS,EBINS,0,EBINS);
@@ -62,6 +62,80 @@ int main(int argc, char **argv){
  		TString name=Form("t%d",i);
  		tspecs[i]=new TH1D(name.Data(),name.Data(),EBINS,0,EBINS);
  	}
+
+ 	//////////////////////////////////////////
+	/// 		  Isomer properties	       ///
+	//////////////////////////////////////////
+
+ 	int gamma_energy_1_134Te = 297;
+	int gamma_energy_2_134Te = 1279;
+
+	//Implement FWHM-dependent gates later
+
+	//////////////////////////////////////////
+	/// 		   Lookup table		  	   ///
+	//////////////////////////////////////////
+
+	//a b c
+	//d e f
+	//g h i
+
+	//true = e - (b+d+f+h)/2 + (a+c+g+i)/4
+	//bg_ridge = (b+d+f+h)/2
+	//bg_random = (a+c+g+i)/4
+
+	//fill whole square with random bg-nr
+	for(int i=gamma_energy_1_134Te-4; i<=gamma_energy_1_134Te+5; i++){
+		for(int j=gamma_energy_2_134Te-4; j<=gamma_energy_2_134Te+5; j++){
+			lookup[i][j] = 3;
+		}
+	}
+
+	//peak gate; e
+	for(int i=gamma_energy_1_134Te-2; i<=gamma_energy_1_134Te+2; i++){
+		for(int j=gamma_energy_2_134Te-2; j<=gamma_energy_2_134Te+2; j++){
+			lookup[i][j] = 2;
+		}
+	}
+
+	//bg-ridge gate, energy_1 i bg_lower; d
+	for(int i=gamma_energy_1_134Te-4; i<=gamma_energy_1_134Te-3; i++){
+		for(int j=gamma_energy_2_134Te-2; j<=gamma_energy_2_134Te+2; j++){
+			lookup[i][j] = 1;
+		}
+	}
+
+	//bg-ridge gate, energy_1 i bg_upper; f
+	for(int i=gamma_energy_1_134Te+3; i<=gamma_energy_1_134Te+5; i++){
+		for(int j=gamma_energy_2_134Te-2; j<=gamma_energy_2_134Te+2; j++){
+			lookup[i][j] = 1;
+		}
+	} 
+
+	//bg-ridge gate, energy_2 i bg_lower; h
+	for(int i=gamma_energy_1_134Te-2; i<=gamma_energy_1_134Te+2; i++){
+		for(int j=gamma_energy_2_134Te-4; j<=gamma_energy_2_134Te-3; j++){
+			lookup[i][j] = 1;
+		}
+	}
+
+	//bg-ridge gate, energy_2 i bg_upper; b
+	for(int i=gamma_energy_1_134Te-2; i<=gamma_energy_1_134Te+2; i++){
+		for(int j=gamma_energy_2_134Te+3; j<=gamma_energy_2_134Te+5; j++){
+			lookup[i][j] = 1;
+		}
+	}
+
+/*	//Print loopup-table to make sure all elements have right value
+	for(int j=gamma_energy_2_134Te-5; j<=gamma_energy_2_134Te+6; j++){
+		for(int i=gamma_energy_1_134Te-5; i<=gamma_energy_1_134Te+6; i++){
+			cout << lookup[i][j] << " ";
+		}
+		cout << "\n";
+	}*/
+
+	
+
 }
 
 //g++ -g -o CubeSort CubeSort.cxx ` root-config --cflags` `root-config --glibs` -lSpectrum
