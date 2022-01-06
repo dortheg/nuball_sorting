ef1c954ab738af25dd87ba1d79350dc2e9dad7a9
Dorthea Gjestvang <dortheagjestvang@Dortheas-MacBook-Air.local>
2022-01-06 10:51:10 +0100

    Managed to move filling of lookuptable to a function

--- 238U/CubeSort.cxx
+++ 238U/CubeSort.cxx
@@ -30,10 +30,8 @@ using namespace std;
 int TBINS=350;
 int EBINS=2048;
 
-short lookup[2048][2048] = {{0}};
 short lookup_134Te[2048][2048] = {{0}};
 int coincidences[20][2];
-int lookup1D[2048] = {0};
 
 //Number of FWHM in the energy gate
 double NFWHM=1.0;
@@ -61,64 +59,15 @@ int main(int argc, char **argv){
  	int gamma_energy_1_134Te = 297;
 	int gamma_energy_2_134Te = 1279;
 
-	//Implement FWHM-dependent gates later
-
 
 	//////////////////////////////////////////
 	/// 		   Lookup table		  	   ///
 	//////////////////////////////////////////
 
-	//a b c
-	//d e f
-	//g h i
-
-	//true = e - (b+d+f+h)/2 + (a+c+g+i)/4
-	//bg_ridge = (b+d+f+h)/2
-	//bg_random = (a+c+g+i)/4
-
-	//fill whole square with random bg-nr
-	for(int i=gamma_energy_1_134Te-4; i<=gamma_energy_1_134Te+5; i++){
-		for(int j=gamma_energy_2_134Te-4; j<=gamma_energy_2_134Te+5; j++){
-			lookup[i][j] = 3;
-		}
-	}
-
-	//peak gate; e
-	for(int i=gamma_energy_1_134Te-2; i<=gamma_energy_1_134Te+2; i++){
-		for(int j=gamma_energy_2_134Te-2; j<=gamma_energy_2_134Te+2; j++){
-			lookup[i][j] = 2;
-		}
-	}
-
-	//bg-ridge gate, energy_1 i bg_lower; d
-	for(int i=gamma_energy_1_134Te-4; i<=gamma_energy_1_134Te-3; i++){
-		for(int j=gamma_energy_2_134Te-2; j<=gamma_energy_2_134Te+2; j++){
-			lookup[i][j] = 1;
-		}
-	}
-
-	//bg-ridge gate, energy_1 i bg_upper; f
-	for(int i=gamma_energy_1_134Te+3; i<=gamma_energy_1_134Te+5; i++){
-		for(int j=gamma_energy_2_134Te-2; j<=gamma_energy_2_134Te+2; j++){
-			lookup[i][j] = 1;
-		}
-	} 
-
-	//bg-ridge gate, energy_2 i bg_lower; h
-	for(int i=gamma_energy_1_134Te-2; i<=gamma_energy_1_134Te+2; i++){
-		for(int j=gamma_energy_2_134Te-4; j<=gamma_energy_2_134Te-3; j++){
-			lookup[i][j] = 1;
-		}
-	}
-
-	//bg-ridge gate, energy_2 i bg_upper; b
-	for(int i=gamma_energy_1_134Te-2; i<=gamma_energy_1_134Te+2; i++){
-		for(int j=gamma_energy_2_134Te+3; j<=gamma_energy_2_134Te+5; j++){
-			lookup[i][j] = 1;
-		}
-	}
+	//Implement FWHM-dependent gates later
 
 	fill_lookuptable(gamma_energy_1_134Te, gamma_energy_2_134Te, lookup_134Te);
+
 
 	//Print loopup-table to make sure all elements have right value
 	for(int j=gamma_energy_2_134Te-5; j<=gamma_energy_2_134Te+6; j++){
@@ -147,20 +96,20 @@ int main(int argc, char **argv){
 				//bg_random = (a+c+g+i)/4
 
 				//e
-				if(lookup[i][j]==2){
+				if(lookup_134Te[i][j]==2){
 					time_isomer_doublegate_134Te->Fill(k, Cube1->Get(i,j,k));
 					time_isomer_doublegate_all_134Te->Fill(k, Cube1->Get(i,j,k));
 				}
 
 				//b, d, f, h
-				if(lookup[i][j]==1){
+				if(lookup_134Te[i][j]==1){
 					time_isomer_doublegate_134Te->Fill(k, -0.5*Cube1->Get(i,j,k));
 					time_isomer_doublegate_bg_134Te->Fill(k, 0.5*Cube1->Get(i,j,k));
 					time_isomer_doublegate_bg_ridge_134Te->Fill(k, 0.5*Cube1->Get(i,j,k));
 				}
 
 				//a, c, g, i
-				if(lookup[i][j]==3){
+				if(lookup_134Te[i][j]==3){
 					time_isomer_doublegate_134Te->Fill(k, 0.25*Cube1->Get(i,j,k));
 					time_isomer_doublegate_bg_134Te->Fill(k, -0.25*Cube1->Get(i,j,k));
 					time_isomer_doublegate_bg_random_134Te->Fill(k, 0.25*Cube1->Get(i,j,k));
@@ -238,8 +187,6 @@ void fill_lookuptable(int energy_1, int energy_2, short lookup[2048][2048]){
 			lookup[i][j] = 1;
 		}
 	}
-
-
 }
 
 
